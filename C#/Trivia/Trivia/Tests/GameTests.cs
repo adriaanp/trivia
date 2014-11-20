e838bb73caf3f64301033077df276f0dc9c58866
Adriaan Putter <adriaan.putter@eoh.co.za>
2014-11-20 14:30:34 +0200

    add test to validate player should stay in penalty box when rolling even number

---
+++ C#/Trivia/Trivia/Tests/GameTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UglyTrivia;
+
+namespace Trivia.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void Add_Name_ShouldIncreasePlayerCount()
+        {
+            var game = new Game();
+            game.add("Adriaan");
+            Assert.AreEqual(1, game.howManyPlayers());
+            game.add("Player 2");
+            Assert.AreEqual(2, game.howManyPlayers());
+        }
+
+        // if in penalty box
+            // if not even roll
+                // get out of penalty box
+                // askQuestion
+            // if even roll
+                // not getting out penalty box
+        // if not in penalty box
+            // move and check location
+            // askQuestion
+        [Test]
+        public void Roll_InPenaltyBoxAndEvenNumber_ShouldNotGetOutOfPenaltyBox()
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                var game = new Game();
+                game.add("Adriaan");
+                game.add("Player 2");
+
+                game.wrongAnswer();
+                game.wrongAnswer();
+
+                game.roll(2);
+
+                var output = writer.GetStringBuilder().ToString();
+                Assert.IsTrue(output.Contains("Adriaan is not getting out of the penalty box"));
+            }
+        }
+    
+    }
+}
